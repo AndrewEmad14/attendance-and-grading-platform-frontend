2e014185c3dcb8b8932b048b5b69885ef7b64233
john-roufaeil <john.f.roufaeil@gmail.com>
2026-06-13 23:48:53 +0300

    fix: add missing PaginatedResponse type

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,24 @@ export interface CohortAdmin {
   email: string | null
 }
 
+export interface PaginatedResponse<T> {
+  data: T[]
+  meta: {
+    current_page: number
+    last_page: number
+    per_page: number
+    total: number
+    from: number
+    to: number
+  }
+  links: {
+    first: string
+    last: string
+    prev: string | null
+    next: string | null
+  }
+}
+
 export interface Cohort {
   id: number
   track_id: number
